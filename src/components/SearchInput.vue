624032383d52692e70424f589b864fd60020cd0b
piotrpopiolek <piotr.popiolek93@gmail.com>
2019-07-10 18:50:34 +0200

    Finish app

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="searchWrapper">
+    <input id="search" name="search" class="{ dark }" :value="value" @input="handleChange" />
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchInput",
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    dark: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    handleChange(event) {
+      this.$emit("input", event.target.value);
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.searchWrapper {
+  input {
+    margin-top: 50px;
+    display: flex;
+    width: 250px;
+    color: white;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 300;
+    height: 30px;
+    border: 0;
+    background: none;
+    border-bottom: 1px solid white;
+    transition: box-shadow 0.3s ease-out;
+    @media (min-width: 1024px) {
+      font-weight: 400;
+    }
+  }
+  input:focus {
+    outline: none;
+    box-shadow: 0 10px 20px -8px rgba(255, 255, 255, 0.5);
+  }
+  .dark {
+    color: #1e3d4a;
+    border-bottom-color: #1e3d4a;
+  }
+  .dark:focus {
+    box-shadow: 0 10px 20px -8px rgba(#1e3d4a, 0.2);
+  }
+}
+</style>
